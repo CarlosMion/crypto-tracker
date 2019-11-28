7033b1d3e64b715bf57e758df7657616045a4f52
Carlos Mion <mion.carlos23@gmail.com>
2019-11-28 18:58:26 -0300

    feature(custom_app_bar) created the custom app bar to fit specifications

--- lib/src/listCryptoCurrencys/screens/cryptos_list.dart
+++ lib/src/listCryptoCurrencys/screens/cryptos_list.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import '../../listCryptoCurrencys/widgets/user_icon.dart';
+import '../../design/custom_app_bar.dart';
+import '../../util/constants.dart';
+
 class CryptosList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('CryptoTracker Pro'),
+      appBar: CustomAppBar(
+        title: APP_NAME,
+        action: UserIcon(),
       ),
     );
   }
---
+++ lib/src/listCryptoCurrencys/widgets/user_icon.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class UserIcon extends StatelessWidget {
+  build(context) {
+    return Image.asset('assets/images/user_icon.png');
+  }
+}
