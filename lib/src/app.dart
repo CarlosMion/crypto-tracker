3e928b0dce7e57b69918b4d6b84bc2b23456f085
Carlos Mion <mion.carlos23@gmail.com>
2019-11-29 00:01:18 -0300

    feature(app)) added provider to the App

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import './routes.dart';
+import './listCryptoCurrencys/bloc/cryptocurrencies_provider.dart';
 
 class App extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CryptoTracker Pro',
-      onGenerateRoute: routes,
+    return CryptocurrenciesProvider(
+      child: MaterialApp(
+        title: 'CryptoTracker Pro',
+        onGenerateRoute: routes,
+      ),
     );
   }
 }
