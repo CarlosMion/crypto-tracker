1f9dbf98c5b9daf76994695fd66fdc9a226d1b8b
Carlos Mion <mion.carlos23@gmail.com>
2019-12-02 12:44:54 -0300

    feature(add_cryptos_bloc) added bloc for add_cryptos screen

--- lib/src/blocs/add_cryptos/add_cryptos_bloc.dart
+++ lib/src/blocs/add_cryptos/add_cryptos_bloc.dart
@@ -2,4 +2,20 @@ import 'dart:async';
 import 'add_cryptos_validators.dart';
 import 'package:rxdart/rxdart.dart';
 
-class AddCryptosBloc extends Object with AddCryptosValidator {}
+class AddCryptosBloc extends Object with AddCryptosValidators {
+  final _cryptoCurrencyController = BehaviorSubject<String>();
+
+  Function(String) get changeCryptocurrency =>
+      _cryptoCurrencyController.sink.add;
+
+  Stream<String> get cryptoCurrency =>
+      _cryptoCurrencyController.stream.transform(validateCryptocurrency);
+
+  submit() {
+    final cryptoCurrency = _cryptoCurrencyController.value;
+  }
+
+  dispose() {
+    _cryptoCurrencyController.close();
+  }
+}
